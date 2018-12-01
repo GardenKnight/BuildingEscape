1ed3737de0e196fe480354e84c6ea5f5bac49ff0
David Hinkle <darkwavepaint@gmail.com>
2018-12-01 11:15:42 -0600

    Added Door Rotation at Begin Play

--- BuildingEscape/Source/BuildingEscape/BuildingEscape.h
+++ BuildingEscape/Source/BuildingEscape/BuildingEscape.h
@@ -1,6 +1,5 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright David Hinkle, Angelica Visione 2018
 
 #pragma once
 
 #include "CoreMinimal.h"
-
---
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,40 @@
+// Copyright David Hinkle, Angelica Visione 2018
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Find the owning Actor
+	AActor* Owner = GetOwner(); // we are declaring a pointer to an actor called Owner
+
+	// Creator a rotator
+	FRotator NewRotation = FRotator(0.0f, -70.0f, 0.0f); // FRotator is a struct declaring variable, calling constructor
+	
+	// Set the door rotation
+	Owner->SetActorRotation(NewRotation); // (NewRotation) is the variable
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
